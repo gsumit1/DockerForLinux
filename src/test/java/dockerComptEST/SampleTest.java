dd21d4ea544e7fd96a661ea75a86b8b23c73a520
Sumit Ghosh <ghosh.sumit1@gmail.com>
2020-10-08 13:44:16 +0530

    MyFirstCommit
    First Code

---
+++ src/test/java/dockerComptEST/SampleTest.java
@@ -0,0 +1,19 @@
+package dockerComptEST;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	final static String userDir = System.getProperty("user.dir");
+
+	@Test
+	public void sample() {
+		 System.setProperty("webdriver.chrome.driver", userDir + "/chromedriver");
+		 WebDriver driver=new ChromeDriver();
+		 driver.get("https://www.google.com/");
+			
+		
+	}
+
+}
