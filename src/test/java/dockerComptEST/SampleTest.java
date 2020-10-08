5a3320d70327ceee875cc53fd290ec0631c4ff48
Sumit Ghosh <ghosh.sumit1@gmail.com>
2020-10-08 19:22:46 +0530

    Update SampleTest.java

--- src/test/java/dockerComptEST/SampleTest.java
+++ src/test/java/dockerComptEST/SampleTest.java
@@ -1,7 +1,8 @@
 package dockerComptEST;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class SampleTest {
@@ -9,12 +10,24 @@ public class SampleTest {
 
 	@Test
 	public void sample() {
-		 System.setProperty("webdriver.chrome.driver","/chromedriver/chromedriver");
-		 WebDriver driver=new ChromeDriver();
-		 driver.get("https://www.google.com/");
-		 System.out.println(driver.getTitle());
-			
-		
+		System.setProperty("webdriver.chrome.driver", "/chromedriver/chromedriver");
+
+		ChromeOptions options = new ChromeOptions();
+
+		System.setProperty("webdriver.chrome.driver", "/chromedriver/chromedriver");
+		options.addArguments("start-maximized"); // open Browser in maximized mode
+		// options.addArguments("disable-infobars"); // disabling infobars
+		options.addArguments("--disable-extensions"); // disabling extensions
+		options.addArguments("--disable-gpu"); // applicable to windows os only
+		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+		options.addArguments("--no-sandbox"); //
+		options.addArguments("--headless"); //
+		ChromeDriverService service = ChromeDriverService.createDefaultService();
+		ChromeDriver driver = new ChromeDriver(service, options);
+
+		driver.get("https://www.google.com/");
+		System.out.println(driver.getTitle());
+
 	}
 
 }
