f2bfd8b8181b3737a04dd0167b0e1cc5f58d4870
Sumit Ghosh <ghosh.sumit1@gmail.com>
2020-10-09 18:51:30 +0530

    Update SampleTest.java

--- src/test/java/dockerComptEST/SampleTest.java
+++ src/test/java/dockerComptEST/SampleTest.java
@@ -12,27 +12,43 @@ public class SampleTest {
 
 	@Test
 	public void sample() {
-		//new File ("/chromedriver/chromedriver").setExecutable(true);
-		System.setProperty("webdriver.chrome.driver", "/chromedriver/chromedriver");
-		
+
+		try {
+			new File("/chromedriver/chromedriver").setExecutable(true);
+			System.out.println("1st block");
+		} catch (Exception e) {
+
+			System.out.println("1st error");
+
+			try {
+				new File("/chromedriver").setExecutable(true);
+				System.out.println("2st block");
+			} catch (Exception e1) {
+
+				System.out.println("2nd error");
+
+			}
+
+		}
+		System.setProperty("webdriver.chrome.driver", "/chromedriver");
+
 		ChromeDriver driver = new ChromeDriver();
 
 		driver.get("https://www.google.com/");
-		
-		
-		
+
 		/*
-		
-		ChromeOptions options = new ChromeOptions();
-		//System.setProperty("webdriver.chrome.driver", "/chromedriver/chromedriver");
-		options.addArguments("start-maximized"); // open Browser in maximized mode
-		// options.addArguments("disable-infobars"); // disabling infobars
-		options.addArguments("--disable-extensions"); // disabling extensions
-		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-		options.addArguments("--no-sandbox"); //
-		options.addArguments("--headless"); //
-		ChromeDriverService service = ChromeDriverService.createDefaultService();
-		ChromeDriver driver = new ChromeDriver(service, options);*/
+		 * 
+		 * ChromeOptions options = new ChromeOptions();
+		 * //System.setProperty("webdriver.chrome.driver",
+		 * "/chromedriver/chromedriver"); options.addArguments("start-maximized"); //
+		 * open Browser in maximized mode // options.addArguments("disable-infobars");
+		 * // disabling infobars options.addArguments("--disable-extensions"); //
+		 * disabling extensions options.addArguments("--disable-dev-shm-usage"); //
+		 * overcome limited resource problems options.addArguments("--no-sandbox"); //
+		 * options.addArguments("--headless"); // ChromeDriverService service =
+		 * ChromeDriverService.createDefaultService(); ChromeDriver driver = new
+		 * ChromeDriver(service, options);
+		 */
 
 		driver.get("https://www.google.com/");
 		System.out.println(driver.getTitle());
