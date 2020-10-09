a064d25f71e85ef166263647b102f1777501b2f0
Sumit Ghosh <ghosh.sumit1@gmail.com>
2020-10-09 18:26:21 +0530

    Update SampleTest.java

--- src/test/java/dockerComptEST/SampleTest.java
+++ src/test/java/dockerComptEST/SampleTest.java
@@ -12,17 +12,13 @@ public class SampleTest {
 
 	@Test
 	public void sample() {
-		new File ("/chromedriver/chromedriver").setExecutable(true);
+		//new File ("/chromedriver/chromedriver").setExecutable(true);
 		System.setProperty("webdriver.chrome.driver", "/chromedriver/chromedriver");
-		
-
 		ChromeOptions options = new ChromeOptions();
-
-		System.setProperty("webdriver.chrome.driver", "/chromedriver/chromedriver");
+		//System.setProperty("webdriver.chrome.driver", "/chromedriver/chromedriver");
 		options.addArguments("start-maximized"); // open Browser in maximized mode
 		// options.addArguments("disable-infobars"); // disabling infobars
 		options.addArguments("--disable-extensions"); // disabling extensions
-		options.addArguments("--disable-gpu"); // applicable to windows os only
 		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 		options.addArguments("--no-sandbox"); //
 		options.addArguments("--headless"); //
